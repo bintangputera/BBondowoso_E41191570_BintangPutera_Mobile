9880545248af66e37e185e7c860087c209a7fda4
Bintang Putera <sangbintang917@gmail.com>
2021-06-08 10:38:26 +0700

    Google Maps API

---
+++ GoogleMapsAPI/app/src/main/java/com/example/googlemapsapi/MapsActivity.kt
@@ -0,0 +1,46 @@
+package com.example.googlemapsapi
+
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.pm.PackageManager
+import android.location.Location
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.example.googlemapsapi.databinding.ActivityMapsBinding
+
+class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+    private lateinit var binding: ActivityMapsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMapsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val mapFragment = supportFragmentManager
+            .findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+        mMap.uiSettings.isZoomControlsEnabled = true
+
+        val sydney = LatLng(-8.1594771, 113.7224597)
+        mMap.addMarker(MarkerOptions().position(sydney).title("Polije Sip"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
+}
